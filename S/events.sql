63e475bb429c3462b22aaae75be3d901eeac837b
SiyiC41 <csyclara0122@gmail.com>
2023-03-28 09:52:16 -0400

    Day 3

--- S/events.sql
+++ S/events.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 27, 2023 at 04:09 PM
+-- Generation Time: Mar 28, 2023 at 03:50 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -48,7 +48,11 @@ CREATE TABLE `events` (
 --
 
 INSERT INTO `events` (`eventId`, `eventName`, `smallPhotoPath`, `largePhotoPath`, `date`, `startTime`, `endTime`, `eventDescription`, `price`, `organizer`, `venue`, `capacity`, `attendeesCount`) VALUES
-(1, 'Arts & Crafts', './images/h1-eventlist-img-1-1-800x593.jpg', './images/event-img-1.jpg', '2023-12-19', '12:30:00', '13:30:00', 'Modus doctus electram ius an, oratio iisque temporibus sed ad, porro interesset mea in. Nullam tempor at vim, usu eu denique hendrerit, impedit suscipiantur ex sit. Nec ne prompta liberavisse. Vis volumus delectus electram ne, eam omnes everti pertinacia ad, stet nonumy nusquam his at.Te sint tincidunt accommodare quo, per appetere inciderint cu, electram corrumpit mei et. Mea ea omnesque dignissim theophrastus, modo alterum honestatis quo id.', '120.00', 'Lance & Stacy Winer', 'Kids room No.01', 10, 0);
+(1, 'Arts & Crafts', '/images/h1-eventlist-img-1-1-800x593.jpg', '/images/event-img-1.jpg', '2023-12-19', '12:30:00', '13:30:00', 'Modus doctus electram ius an, oratio iisque temporibus sed ad, porro interesset mea in. Nullam tempor at vim, usu eu denique hendrerit, impedit suscipiantur ex sit. Nec ne prompta liberavisse. Vis volumus delectus electram ne, eam omnes everti pertinacia ad, stet nonumy nusquam his at.Te sint tincidunt accommodare quo, per appetere inciderint cu, electram corrumpit mei et. Mea ea omnesque dignissim theophrastus, modo alterum honestatis quo id.', '120.00', 'Lance & Stacy Winer', 'Kids room No.01', 10, 0),
+(2, 'Trampolines', '/images/h1-eventlist-img-2-800x593.jpg', '/images/event-img-2.jpg', '2023-11-17', '08:00:00', '10:00:00', 'Graecis corrumpit comprehensam eum ut, dolores accusamus salutatus ea ius. Mel assum tincidunt vituperatoribus ex, enim consectetuer ea eam, et ludus affert pertinax mei. Impedit ceteros te his, meis eirmod eum an. Te sit legimus albucius. Vivendo ocurreret at pri, lobortis antiopam pro at. Vitae oratio ex est, vim cu animal intellegam ullamcorper, apeirian dissentiet an cum. Cum habeo facilis postulant ne. Meliore recteque pri, debet urbanitas necessitatibus.', '45.00', 'Melani & Mark Hamp', 'Kids room Garden', 10, 0),
+(3, 'Halloween Party', '/images/h1-eventlist-img-3-800x593.jpg', '/images/event-img-3.jpg', '2023-10-30', '12:00:00', '22:00:00', 'Lorem ipsum dolor sit amet, cum te decore alterum admodum. Te qui doctus voluptaria inciderint, in mel probo neglegentur. Causae corrumpit laboramus sed at. Usu hinc dolore erroribus ut, eos id bonorum propriae torquatos. Aliquam expetenda reformidans in mei. Cum te legere tamquam explicari. Has feugiat veritus eu, eum tritani laores ancillae at, mundi homero domerum in est. Vix at nonumy honestatis, ne ego lorem facilisis pertinacia his expetenda propriae torquatos.', '180.00', 'Angela & Nate Lopez', 'Kids room No.03', 10, 0),
+(4, 'Slides', '/images/h1-eventlist-img-4-800x593.jpg', '/images/event-img-4.jpg', '2023-09-18', '15:00:00', '18:00:00', 'Vis tota vivendo disputationi ei. Ius veri lorem sanctus ne. Sed te malis eruditi, his in affert delicata, cetero quaestio an ius. Te aeque laoreet ius. Hinc soleat vix ad, eum id tale etiam salutandi. Te sint tincidunt accommodare quo, per appetere inciderint cu, electram corrumpit mei et. Mea ea omnesque dignissim theophrastus, modo alterum honestatis quo id. Vix possit torquatos vulputate ad, et vix tota definitiones. Ut dicant disputationi placerat concludaturque vix.', '45.00', 'Jon & Natasha Shade', 'Kids room Garden', 10, 0),
+(5, 'Science', '/images/h1-eventlist-img-5-800x593.jpg', '/images/event-img-5.jpg', '2023-08-22', '08:00:00', '16:00:00', 'Nullam tempor at vim, usu eu denique hendrerit, impedit suscipiantur ex sit. Nec ne prompta liberavisse. Vis volumus delectus electram ne, eam omnes evertim pertinacia ad, stet nonumy nusquam his at. Ei clita saperet argumentum mea, qui ipsum homero reprimique in. Suas quas venimam at eam, vix an vide deleniti, oblique placerat pri no. Vel id prima reprimique dissentiet, eam cu semper albucius. Sed no posse semper. In popul legimus vocibus, doctus animal eu.', '285.00', 'Lance & Stacy Winer', 'Kids room No.04', 10, 0);
 
 --
 -- Indexes for dumped tables
@@ -69,7 +73,7 @@ ALTER TABLE `events`
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `eventId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `eventId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- S/kids_event_web_Siyi/index.php
+++ S/kids_event_web_Siyi/index.php
@@ -8,6 +8,15 @@ use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/vendor/autoload.php';
 
+DB::$user = 'playroom';
+DB::$password = 'LCjs-BDv_mW8j(0*';
+DB::$dbName = 'playroom';
+DB::$host = 'localhost';
+DB::$port = '3306';
+
+// DB::$user = 'playroom';
+// DB::$password = 'Qh_83o2JKeocBABm';
+
 // Create Container
 $container = new Container();
 AppFactory::setContainer($container);
@@ -23,11 +32,6 @@ $app = AppFactory::create();
 // Add Twig-View Middleware
 $app->add(TwigMiddleware::createFromContainer($app));
 
-DB::$user = 'playroom';
-DB::$password = 'Qh_83o2JKeocBABm';
-DB::$dbName = 'playroom';
-DB::$host = 'localhost';
-
 // Get home page
 $app->get('/', function ($request, $response, $args) {
   return $this->get('view')->render($response, 'home.html.twig');
@@ -35,13 +39,39 @@ $app->get('/', function ($request, $response, $args) {
 
 // Get event page
 $app->get('/event', function ($request, $response, $args) {
-  // // Fetch all events from the database
-  // $events = DB::query('SELECT * FROM events');
+  // Fetch all events from the database
+  $events = DB::query('SELECT * FROM events');
+  foreach ($events as &$event) {
+    $startTime = new DateTime($event['startTime']);
+    $event['startTime'] = $startTime->format('g:i A');
+  }
+  foreach ($events as &$event) {
+    $endTime = new DateTime($event['endTime']);
+    $event['endTime'] = $endTime->format('g:i A');
+  }
+  foreach ($events as &$event) {
+    $date = new DateTime($event['date']);
+    $event['date'] = $date->format('M d');
+  }
+  // Render the events page using the events data
+  return $this->get('view')->render($response, 'event.html.twig', ['events' => $events]);
+});
 
-  // // Render the events page using the events data
-  // return $this->get('view')->render($response, 'events.html.twig', ['events' => $events]);
-  
-  return $this->get('view')->render($response, 'event.html.twig');
+// Get event information pages
+$app->get('/event/{id}', function ($request, $response, $args) {
+  $eventId = $args['id']; 
+  // Fetch the event details from the database using the event ID 
+  $event = DB::queryFirstRow('SELECT * FROM events WHERE eventId=%i',
+  $eventId); 
+  $startTime = new DateTime($event['startTime']);
+  $event['startTime'] = $startTime->format('g:i A');
+  $endTime = new DateTime($event['endTime']);
+  $event['endTime'] = $endTime->format('g:i A');
+  $date = new DateTime($event['date']);
+  $event['date'] = $date->format('y M d');
+  // Render the event detail page using the event data return
+  return $this->get('view')->render($response, 'eventinformation.html.twig', ['event' =>
+  $event]); 
 });
 
 // Get about us page
