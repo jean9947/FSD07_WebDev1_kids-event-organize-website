bd2f14114d1fdef222cc3fb9dc923bc843dd6439
jean9947 <jean7499@gmail.com>
2023-03-29 19:44:28 -0400

    CRUD: users, bookings DONE/TESTED

--- C/admin.php
+++ C/admin.php
@@ -51,17 +51,11 @@ $app->get('/admin/users', function($request, $response) {
 /** ADD users */
 // STATE 1: first display of the form
 $app->get('/admin/adduser', function ($request, $response, $args) {
-    // Check if user is authenticated
-    if (!isset($_SESSION['user'])) {
-        setFlashMessage("Please log in to edit.");
-        return $response
-            ->withHeader('Location', '/login')
-            ->withStatus(302);
-    }
-    $username = $_SESSION['user']['username'];
-    $isAdmin = ($_SESSION['user']['role'] === 'admin');
-    return $this->get('view')->render($response, 'admin_adduser.html.twig', ['username' => $username, 'isAdmin' => $isAdmin]);
-    // return $this->get('view')->render($response, 'admin_adduser.html.twig');
+    $users = DB::query("SELECT * FROM users");
+    $html = $this->get('view')->fetch('admin_adduser.html.twig', [
+        'users' => $users]);
+    $response->getBody()->write($html);
+    return $response->withHeader('Content-Type', 'text/html');
 });
 
 // SATE 2&3: receiving a submission
@@ -76,82 +70,73 @@ $app->post('/admin/adduser', function ($request, $response, $args) {
     $email = $data['email'];
     
     $errorList = [];
-    // validate firstname
-    if (strlen($firstName) < 2 || strlen($firstName) > 100) {
-        $errorList []= "First name must be 2-100 characters long";
-        $firstName = "";
-    }
-    // validate lastname
-    if (strlen($lastName) < 2 || strlen($lastName) > 100) {
-        $errorList []= "Last name must be 2-100 characters long";
-        $lastName = "";
-    }
-    // validate username and check if it's taken
-    if (preg_match('/^[a-z][a-z0-9_]{3,19}$/', $username) !== 1) {
-        $errorList[] = "Username must be made up of 4-20 letters, digits, or underscore. The first character must be a letter";
-        $username = "";
-    } else {
-        $userRecord = DB::queryFirstRow("SELECT * FROM users WHERE username=%s", $username);
-        if ($userRecord) {
-            $errorList[] = "This username is already registered";
+
+    if(isset($_SESSION['user'])) {
+        $userId = $_SESSION['user']['userId'];
+        // validate firstname
+        if (strlen($firstName) < 2 || strlen($firstName) > 100) {
+            $errorList []= "First name must be 2-100 characters long";
+            $firstName = "";
+        }
+        // validate lastname
+        if (strlen($lastName) < 2 || strlen($lastName) > 100) {
+            $errorList []= "Last name must be 2-100 characters long";
+            $lastName = "";
+        }
+        // validate username and check if it's taken
+        if (preg_match('/^[a-z][a-z0-9_]{3,19}$/', $username) !== 1) {
+            $errorList[] = "Username must be made up of 4-20 letters, digits, or underscore. The first character must be a letter";
             $username = "";
-          }
-    }
+        } else {
+            $userRecord = DB::queryFirstRow("SELECT * FROM users WHERE username=%s", $username);
+            if ($userRecord) {
+                $errorList[] = "This username is already registered";
+                $username = "";
+            }
+        }
 
-    // validate password
-    if (
-        strlen($password) < 6 || strlen($password) > 100
-        || (preg_match("/[A-Z]/", $password) !== 1)
-        || (preg_match("/[a-z]/", $password) !== 1)
-        || (preg_match("/[0-9]/", $password) !== 1)
-    ) {
-        $errorList[] = "Password must be 6-100 characters long and contain at least one uppercase letter, one lowercase, and one digit.";
-        $password = "";
-    }
-    // validate phone
-    if (preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $phoneNumber) !== 1) {
-        $errorList[] ="Phone number format is 000-000-0000";
-        $phoneNumber = "";
-    }
-    // validate email
-    if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
-        $errorList[] = "Email does not look valid";
-        $email = "";
-    }
-
-
-    if ($errorList) { // STATE 2: errors
+        // validate password
+        if (
+            strlen($password) < 6 || strlen($password) > 100
+            || (preg_match("/[A-Z]/", $password) !== 1)
+            || (preg_match("/[a-z]/", $password) !== 1)
+            || (preg_match("/[0-9]/", $password) !== 1)
+        ) {
+            $errorList[] = "Password must be 6-100 characters long and contain at least one uppercase letter, one lowercase, and one digit.";
+            $password = "";
+        }
+        // validate phone
+        if (preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $phoneNumber) !== 1) {
+            $errorList[] ="Phone number format is 000-000-0000";
+            $phoneNumber = "";
+        }
+        // validate email
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+            $errorList[] = "Email does not look valid";
+            $email = "";
+        }
+        if ($errorList) { // STATE 2: errors
         $valuesList = ['firstName' => $firstName, 'lastName' => $lastName, 'username' => $username, 'password' => $password, 
                         'phoneNumber' => $phoneNumber, 'email' => $email, 'role' => $role];
         return $this->get('view')->render($response, 'admin_adduser.html.twig', ['errorList' => $errorList, 'v' => $valuesList]);
-    } else { // STATE 3: sucess - add new user to the DB
-        DB::insert('users', ['userId' => NULL, 'username' => $username, 'firstName' => $firstName, 'lastName' => $lastName, 
-        'password' => $password, 'phoneNumber' => $phoneNumber, 'email' => $email, 'role' => $role]);
-        return $this->get('view')->render($response, 'admin_users.html.twig');
-        // setFlashMessage("user added, redirecting to the admin page...");
-        // return $response->withRedirect("/admin");   
-    }
-})->setName('addUser');
+        } else { // STATE 3: sucess - add new user to the DB
+            DB::insert('users', ['userId' => NULL, 'username' => $username, 'firstName' => $firstName, 'lastName' => $lastName, 
+            'password' => $password, 'phoneNumber' => $phoneNumber, 'email' => $email, 'role' => $role]);
+            return $response->withHeader('Location', '/admin/users')->withStatus(302);
+        }
+    } else {
+        return $response->withHeader('Location', '/login')->withStatus(302);
+      }
+});
 
 /*************************************************************** */
 
 /** DELETE user */
-// $app->post('/admin/users', function ($request, $response, $args) {
-//     $userId = $request->getParam('userId');
-//     DB::delete('users', 'userId=%d', $userId);
-//     return $response->withHeader('Location', '/admin/users')->withStatus(302);
-// });
-$app->post('/admin/users', function ($request, $response, $args) {
-    // Check if user is authenticated
-    if (!isset($_SESSION['user'])) {
-        setFlashMessage("Please log in to edit.");
-        return $response
-            ->withHeader('Location', '/login')
-            ->withStatus(302);
-    }
-    $userId = $request->getParam('userId');
+$app->delete('/admin/users/{userId}', function ($request, $response, $args) {
+    // $userId = $request->getParam('userId');
+    $userId = $args['userId'];
     DB::delete('users', 'userId=%d', $userId);
-    return $response->withJson(['status' => 'success']);
+    return $this->get('view')->render($response, 'admin_users.html.twig');
 });
 
 /*************************************************************** */
@@ -223,10 +208,12 @@ $app->post('/admin/updateuser/{userId}', function ($request, $response, $args) {
         || (preg_match("/[0-9]/", $password) !== 1)
     ) {
         $errorList[] = "Password must be 6-100 characters long and contain at least one uppercase letter, one lowercase, and one digit.";
+        $password = "";
     }
     // validate phone
     if (preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $phoneNumber) !== 1) {
         $errorList[] ="Phone number format is 000-000-0000";
+        $phoneNumber = "";
     }
     // validate email
     if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
@@ -236,20 +223,13 @@ $app->post('/admin/updateuser/{userId}', function ($request, $response, $args) {
 
 
     if ($errorList) { // STATE 2: errors
-        $valuesList = ['firstName' => $firstName, 
-                        'lastName' => $lastName, 
-                        'username' => $username, 
-                        'password' => $password, 
-                        'phoneNumber' => $phoneNumber, 
-                        'email' => $email,];
+        $valuesList = ['username' => $username, 'firstName' => $firstName, 'lastName' => $lastName, 
+        'password' => $password, 'phoneNumber' => $phoneNumber, 'email' => $email];
         return $this->get('view')->render($response, 'admin_updateuser.html.twig', ['errorList' => $errorList, 'v' => $valuesList]);
     } else { // STATE 3: sucess - update the user from the database
     DB::update('users', ['username' => $username, 'firstName' => $firstName, 'lastName' => $lastName, 
     'password' => $password, 'phoneNumber' => $phoneNumber, 'email' => $email]);
-    // Display a success message
-    // return $this->get('view')->render($response, 'admin_users.html.twig');
-    setFlashMessage("user updated, redirecting...");
-    return $response->withRedirect("/admin/users");   
+    return $this->get('view')->render($response, 'admin_users.html.twig');
     }
 });
 
@@ -258,13 +238,112 @@ $app->post('/admin/updateuser/{userId}', function ($request, $response, $args) {
 /************************************** Bookings - CRUD ************************************************** */
 
 /** VIEW all bookings */
+$app->get('/admin/bookings', function($request, $response) {
+    $username = $_SESSION['user']['username'];
+    $isAdmin = ($_SESSION['user']['role'] === 'admin');
+    // Check if user is authenticated
+    if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
+        setFlashMessage("Admin must log in to edit.");
+        return $response
+            ->withHeader('Location', '/login')
+            ->withStatus(302);
+    }
+    $booking = DB::query("SELECT bookingId, eventId , userId, childId, bookingTimeStamp FROM bookings");
+    return $this->get('view')->render($response, 'admin_bookings.html.twig', ['username' => $username, 'isAdmin' => $isAdmin, 'bookings' => $booking]);
+});
+
 
 /** ADD booking */
+$app->get('/admin/addbooking', function ($request, $response, $args) {
+    $bookings = DB::query("SELECT * FROM bookings");
+    $html = $this->get('view')->fetch('admin_addbooking.html.twig', [
+        'bookings' => $bookings]);
+    $response->getBody()->write($html);
+    return $response->withHeader('Content-Type', 'text/html');
+});
+
+$app->post('/admin/addbooking', function ($request, $response, $args) {
+    $data = $request->getParsedBody();
+    $eventId = $data['eventId'];
+    $userId = $data['userId'];
+    $childId = $data['childId'];
+
+    $errorList = [];
+
+    if(isset($_SESSION['user'])) {
+        if (!$eventId || !$userId || !$childId) {
+            $errorList []= "Please fill in all";
+          }
+        if ($errorList) { // STATE 2: errors
+        $valuesList = ['eventId' => $eventId, 'userId' => $userId, 'childId' => $childId];
+        return $this->get('view')->render($response, 'admin_addbooking.html.twig', ['errorList' => $errorList, 'v' => $valuesList]);
+        } else { // STATE 3: sucess - add new user to the DB
+            DB::insert('bookings', ['bookingId' => NULL, 'eventId' => $eventId, 'userId' => $userId, 'childId' => $childId]);
+            return $response->withHeader('Location', '/admin/bookings')->withStatus(302);
+        }
+    } else {
+        return $response->withHeader('Location', '/login')->withStatus(302);
+      }
+});
+
 
 /** DELETE booking */
+$app->delete('/admin/bookings/{bookingId}', function ($request, $response, $args) {
+    $bookingId = $args['bookingId'];
+    DB::delete('bookings', 'bookingId=%d', $bookingId);
+    return $this->get('view')->render($response, 'admin_bookings.html.twig');
+});
+
 
 /** UPDATE booking */
+$app->get('/admin/updatebooking/{bookingId}', function ($request, $response, $args) {
+    // Check if user is authenticated
+    if (!isset($_SESSION['user'])) {
+        setFlashMessage("Please log in to edit.");
+        return $response
+            ->withHeader('Location', '/login')
+            ->withStatus(302);
+    }
+    $username = $_SESSION['user']['username'];
+    $isAdmin = ($_SESSION['user']['role'] === 'admin');
 
+    $events = DB::query("SELECT * FROM events");
+    $bookingId = $args['bookingId'];
+    // Get the user record based on the provided id
+    $bookingRecord = DB::queryFirstRow("SELECT * FROM bookings WHERE bookingId=%d", $bookingId);
+    if (!$bookingRecord) {
+        $response->getBody()->write("Error: booking not found");
+    }
+    return $this->get('view')->render($response, 'admin_updatebooking.html.twig', ['username' => $username, 'isAdmin' => $isAdmin, 
+    'bookingRecord' => $bookingRecord]);
+});
+
+$app->post('/admin/updatebooking/{bookingId}', function ($request, $response, $args) {
+    $bookingId = $args['bookingId'];
+    // Get the user record based on the provided id
+    $bookingRecord = DB::queryFirstRow("SELECT * FROM bookings WHERE bookingId=%d", $bookingId);
+    if (!$bookingRecord) {
+        $response->getBody()->write("Error: booking not found");
+    }
+
+    $data = $request->getParsedBody();
+    $eventId = $data['eventId'];
+    $userId = $data['userId'];
+    $childId = $data['childId'];
+    
+    $errorList = [];
+
+    if (!$eventId || !$userId || !$childId) {
+        $errorList []= "Please fill in all";
+      }
+    if ($errorList) { // STATE 2: errors
+    $valuesList = ['eventId' => $eventId, 'userId' => $userId, 'childId' => $childId];
+    return $this->get('view')->render($response, 'admin_addbooking.html.twig', ['errorList' => $errorList, 'v' => $valuesList]);
+    } else { // STATE 3: sucess - add new user to the DB
+        DB::update('bookings', ['eventId' => $eventId, 'userId' => $userId, 'childId' => $childId]);
+        return $response->withHeader('Location', '/admin/bookings')->withStatus(302);
+    }
+});
 
 
 
@@ -281,9 +360,14 @@ $app->get('/admin/events', function($request, $response) {
             ->withHeader('Location', '/login')
             ->withStatus(302);
     }
-    $events = DB::query("SELECT eventId, eventName, date, starttime, endtime, price, organizer, venue, capacity, attendeesCount FROM events");
-    return $this->get('view')->render($response, 'admin_events.html.twig', ['username' => $username, 'isAdmin' => $isAdmin, 'events' => $events]);
+    $event = DB::query("SELECT eventId, eventName, date, price, capacity, attendeesCount FROM events");
+    return $this->get('view')->render($response, 'admin_events.html.twig', ['username' => $username, 'isAdmin' => $isAdmin, 'events' => $event]);
 });
 
 
-/*************************************************************** */
+/** DELETE event */
+$app->delete('/admin/events/{eventId}', function ($request, $response, $args) {
+    $bookingId = $args['eventId'];
+    DB::delete('events', 'eventId=%d', $eventId);
+    return $this->get('view')->render($response, 'admin_events.html.twig');
+});
--- C/user.php
+++ C/user.php
@@ -134,7 +134,7 @@ $app->post('/login', function (Request $request, Response $response, $args) {
     } elseif ($loginSuccessful) { // logged in as a customer
         unset($userRecord['password']);
         $_SESSION['user'] = $userRecord;
-        return $this->get('view')->render($response, 'home.html.twig'); 
+        return $this->get('view')->render($response, 'loggedin.html.twig');
     } else {
         $response->getBody()->write("Invalid username or password");
         return $reponse;
@@ -152,3 +152,126 @@ $app->get('/logout', function ($request, $response, $args) {
         ->withStatus(302);
     // return $this->get('view')->render($response, 'logout.html.twig');
 })->setName('logout');
+
+
+/************************************** Customers - CRUD ************************************************** */
+
+// Get event page
+$app->get('/event', function ($request, $response, $args) {
+    // Fetch all events from the database
+    $events = DB::query('SELECT * FROM events');
+    foreach ($events as &$event) {
+      $startTime = new DateTime($event['startTime']);
+      $event['startTime'] = $startTime->format('g:i A');
+    }
+    foreach ($events as &$event) {
+      $endTime = new DateTime($event['endTime']);
+      $event['endTime'] = $endTime->format('g:i A');
+    }
+    foreach ($events as &$event) {
+      $date = new DateTime($event['date']);
+      $event['date'] = $date->format('M d');
+    }
+    // Render the events page using the events data
+    return $this->get('view')->render($response, 'event.html.twig', ['events' => $events]);
+  });
+  
+  // Get event information pages
+  $app->get('/event/{id}', function ($request, $response, $args) {
+    $eventId = $args['id']; 
+    // Fetch the event details from the database using the event ID 
+    $event = DB::queryFirstRow('SELECT * FROM events WHERE eventId=%i',
+    $eventId); 
+    $startTime = new DateTime($event['startTime']);
+    $event['startTime'] = $startTime->format('g:i A');
+    $endTime = new DateTime($event['endTime']);
+    $event['endTime'] = $endTime->format('g:i A');
+    $date = new DateTime($event['date']);
+    $event['date'] = $date->format('y M d');
+    // Render the event detail page using the event data return
+    return $this->get('view')->render($response, 'eventinformation.html.twig', ['event' =>
+    $event]); 
+  });
+  
+  // Get about us page
+  $app->get('/aboutus', function ($request, $response, $args) {
+    return $this->get('view')->render($response, 'aboutus.html.twig');
+  });
+  
+  // Get gallery page
+  $app->get('/gallery', function ($request, $response, $args) {
+    return $this->get('view')->render($response, 'gallery.html.twig');
+  });
+  
+  // Get my bookings page
+  $app->get('/mybookings', function ($request, $response, $args) {
+    return $this->get('view')->render($response, 'mybookings.html.twig');
+  });
+  
+  
+  $app->get('/booking-form', function( $request, $response, $args) {
+    $events = DB::query("SELECT * FROM events");
+    $html = $this->get('view')->fetch('booking_form.html.twig', [
+      'events' => $events]);
+    $response->getBody()->write($html);
+    return $response->withHeader('Content-Type', 'text/html');
+  });
+  
+  
+  $app->post('/booking-form', function ($request, $response, $args) {
+    $data = $request->getParsedBody();
+    $KfirstName = $data['firstName'];
+    $KlastName =  $data['lastName'];
+    $birthday = $data['birthday'];
+    $eventId = $data['eventId'];
+    $gender = $data['gender'];
+    
+    $errorList = [];
+  
+    if(isset($_SESSION['user'])) {
+  
+      $userId = $_SESSION['user']['userId'];
+  
+      if (strlen($KfirstName) < 2 || strlen($KfirstName) > 100) {
+        $errorList []= "First name must be 2-100 characters long";
+        $KfirstName = "";
+      }
+      
+      if (strlen($KlastName) < 2 || strlen($KlastName) > 100) {
+        $errorList []= "Last name must be 2-100 characters long";
+        $KlastName = "";
+      }
+  
+      $age = date_diff(date_create($birthday), date_create('now'))->y;
+      if ($age < 2) {
+        $errorList []= "Your child must be over 2 years old";
+        $birthday = "";
+      }
+  
+      if ($errorList) {
+        $valuesList = ['firstName' => $KfirstName, 
+                        'lastName' => $KlastName, 
+                        'birthday' => $birthday, 
+                      ];
+        return $this->get('view')->render($response, 'booking_form.html.twig', ['errorList' => $errorList, 'v' => $valuesList]);
+      } else {
+        DB::insert('children', [
+          'userId' => $userId,
+          'firstName' => $KfirstName,
+          'lastName' => $KlastName,
+          'DOB' => $birthday,
+          'gender' => $gender
+        ]);
+        $childId = DB::queryFirstField("SELECT LAST_INSERT_ID() FROM children");
+        DB::insert('bookings', [
+          'eventId' => $eventId,
+          'userId' => $userId,
+          'childId' => $childId
+        ]);
+        return $response->withHeader('Location', '/mybookings')->withStatus(302);
+      }
+    }else{
+      return $response->withHeader('Location', '/login')->withStatus(302);
+    }
+  });
+  
